c4c5be5736e0200640ef8ef8d73e5013c13f88e1
Loïc Le Goas <legoas.loic@gmail.com>
2020-03-03 15:59:03 +0100

    continuation td3

--- app/utils/Contacts.js
+++ app/utils/Contacts.js
@@ -1,22 +1,29 @@
-import { filterBy } from '@ember/object/computed';
+import { filterBy, alias } from '@ember/object/computed';
 import EmberObject,{computed} from '@ember/object';
 
 export default EmberObject.extend({
-  init:function(){
-    var filtre = '';
+  init(params){
+    this._super(...arguments);
+    this.set('datas',params);
   },
-  contacts:computed('filtre','datas.@each.isDeleted',function(){
-      return this.get('store').query('contact',{
-        filter:{
-          isDeleted:false,
-          name:this.get('filtre')
-        }
-      })
-    }),
+  contacts:computed('datas.@each.isDeleted','filter',function(){
+      let notDeleted = this.get('datas').filterBy('isDeleted', false);
+      if(this.get('filter')){
+        return notDeleted.filter((item)=> {
+          let filter = this.get('filter');
+          return item.get('nom').includes(filter) || item.get('prenom').includes(filter) || item.get('email').includes(filter);
+        })
+      }
+    return notDeleted;
+
+  }),
+
   deleteds: filterBy('datas', 'isDeleted', true)
+
   ,
-  deletedsCount:computed('deleteds',function(){
+    deletedsCount: alias('deleteds.length')
+  /*deletedsCount:computed('deleteds',function(){
     return this.get('deleteds').length
-    }
-  )
+    }*/
+
 });
