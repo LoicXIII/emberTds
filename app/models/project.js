529b6850e4b17342bf311d0b3524fee041515be3
Loïc Le Goas <legoas.loic@gmail.com>
2020-03-10 17:08:16 +0100

    avant Gestion des stories par projet

--- app/models/project.js
+++ app/models/project.js
@@ -4,6 +4,7 @@ const { Model } = DS;
 export default Model.extend({
   name:DS.attr('string'),
   descriptif:DS.attr('string'),
-  startDate:DS.attr('string'),
-  dueDate:DS.attr('string'),
+  startDate:DS.attr('utc'),
+  dueDate:DS.attr('utc'),
+  owner:DS.attr('string'),
 });
