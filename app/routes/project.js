2c192571b9868f40e309578a3a929e2e993aebfb
Loïc Le Goas <legoas.loic@gmail.com>
2020-03-24 17:26:16 +0100

    debut de création new pour les stories

--- app/routes/project.js
+++ app/routes/project.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+import developer from "../models/developer";
 
 export default Route.extend({
   model() {
     let {project_id}=this.paramsFor('project');
     return RSVP.hash({
-      pro:project_id,
       stories: this.store.query('story',{filter: {project:project_id}}),
       project:this.store.findAll('project'),
     });
--- app/routes/story/new.js
+++ app/routes/story/new.js
@@ -1,15 +1,23 @@
 import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  // model() {
+  //   return EmberObject.create(
+  //     {
+  //       projectObject:EmberObject.create(),
+  //       developers:this.get('store').findAll('developer'),
+  //       project:this.get('store').findAll('project')
+  //     }
+  //   );  },
   model() {
-    return EmberObject.create(
-      {
-        projectObject:EmberObject.create(),
-        developers:this.get('store').findAll('developer'),
-        project:this.get('store').findAll('project')
-      }
-    );  },
+    let {project_id}=this.paramsFor('project');
+    return RSVP.hash({
+      stories: this.store.query('story',{filter: {project:project_id}}),
+      project:this.store.findAll('project'),
+    });
+  },
   actions:{
     save:function(identite){
       let store=this.get('store');
